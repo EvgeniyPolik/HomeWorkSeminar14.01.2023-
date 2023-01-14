e2a37080dd4cf8975ee490f71ec343835e06e847
poliex <poliex86@gmail.com>
2023-01-14 21:43:22 +0500

    Task 3 complete

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите проверяемое число: ");
+try
+{
+    var n = int.Parse(Console.ReadLine() ?? "");
+    if (n%2 == 0)
+        Console.WriteLine($"Число {n} - четное");
+    else 
+        Console.WriteLine($"Число {n} - нечетное");
+}
+catch
+{
+    Console.WriteLine("Введено не число!");
+}
