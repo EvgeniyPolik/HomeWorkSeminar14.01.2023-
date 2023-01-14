8e7c081d4c3ec427a4829d8470b493d2a0598a58
poliex <poliex86@gmail.com>
2023-01-14 21:12:38 +0500

    Task 1 complete

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Dictionary<bool, string> variants = new  Dictionary<bool, string>
+{
+    {false, " не "},
+    {true, " "}
+};
+try
+{
+    Console.Write("Введите первое число: ");
+    var a = int.Parse(Console.ReadLine() ?? "0");
+    Console.Write("Введите второе число: ");
+    var b = int.Parse(Console.ReadLine() ?? "0");
+    Console.WriteLine($"Число {a}{variants[a == b*b]}является квадратом числа {b}");
+}
+catch 
+{
+    Console.WriteLine("Введеные значения должны быть числами!");
+}
