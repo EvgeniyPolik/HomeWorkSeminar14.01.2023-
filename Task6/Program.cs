93f4b8cc986b16f87fb9686b4595f5ec0f5add49
poliex <poliex86@gmail.com>
2023-01-14 22:47:25 +0500

    Task 6 complete

---
+++ Task6/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+try
+{
+    var str = (Console.ReadLine() ?? "");
+    int result;
+    if (str.Length == 3)
+    {
+        int number = int.Parse(str);
+        result = number / 10;
+        result %= 10;
+    }
+    else
+        throw new ArgumentException();
+    Console.WriteLine(result);
+}
+catch
+{
+    Console.WriteLine("Необходимо ввести трехзначное число!");
+}
