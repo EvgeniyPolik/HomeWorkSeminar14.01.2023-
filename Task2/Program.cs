c9d69ad1d40876283e3ab7c689c216739136cc74
poliex <poliex86@gmail.com>
2023-01-14 21:31:25 +0500

    Task 2 complete

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Dictionary<int, string> dayOfWeek = new Dictionary<int, string>{
+    {1, "Понедельник"},
+    {2, "Вторник"},
+    {3, "Среда"},
+    {4, "Четверг"},
+    {5, "Пятница"},
+    {6, "Суббота"},
+    {7, "Воскресение"},
+};
+Console.Write("Введите номер дня: ");
+try
+{
+    var numberDay = int.Parse(Console.ReadLine() ?? "");
+    Console.WriteLine($"Выбранный день недели: {dayOfWeek[numberDay]}");
+}
+catch
+{
+    Console.WriteLine("Необходимо ввести число от 1 до 7 включительно");
+}
