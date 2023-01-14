85b81ecf323849351102f8602fe5e6dacb593d24
poliex <poliex86@gmail.com>
2023-01-14 21:47:55 +0500

    Task 4 complete

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите проверяемое число: ");
+try
+{
+    var n = int.Parse(Console.ReadLine() ?? "");
+    for (int i = 1; i <= n; i++)
+    if (i%2 == 0)
+        Console.Write(i + " ");
+}
+catch
+{
+    Console.WriteLine("Введено не число!");
+}
