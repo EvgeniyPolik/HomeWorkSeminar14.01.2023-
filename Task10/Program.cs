967bb690336be028144779aa8028be2691676011
poliex <poliex86@gmail.com>
2023-01-14 23:56:07 +0500

    Task 10 complete

---
+++ Task10/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+try
+{
+    var str = (Console.ReadLine() ?? "");
+    string result;
+    if (str.Length == 3)  // Убедимся что введено имено трехзначное число
+    {
+        int number = int.Parse(str);
+        result = str.Substring(2, 1);
+    }
+    else
+        throw new ArgumentException();
+    Console.WriteLine($"Третья цифра числа {str} это: {result}");
+}
+catch
+{
+    Console.WriteLine("Введено не трехзначное число!");
+}
