d92eb40e4c51d520e0a4740e896bf6cff57cbbef
poliex <poliex86@gmail.com>
2023-01-14 22:04:43 +0500

    Task 5 complete

---
+++ Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+try
+{
+    var number = int.Parse(Console.ReadLine().Substring(2) ?? "");
+    Console.WriteLine(number);
+}
+catch
+{
+    Console.WriteLine("Необходимо ввести трехзначное число!");
+}
